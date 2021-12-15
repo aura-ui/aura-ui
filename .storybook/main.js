a17663eddf54098689e39f9ea5baba62f4081b60
Greg Ogun <gregogun97@gmail.com>
2021-12-15 21:29:45 +0000

    chore(storybook): point stories path to packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../packages/**/stories/*.stories.mdx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
 };
