3734b2b73b57892a76c69dcf272ca5db954d8907
Greg Ogun <gregogun97@gmail.com>
2021-12-15 22:48:25 +0000

    chore(storybook): add stories path for layout structure

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../packages/**/stories/*.stories.mdx'],
+  stories: ['../packages/**/stories/*.stories.tsx', '../packages/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
 };
