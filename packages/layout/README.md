eb3a7347240b6f01b792dc5fa9aedb8c66839219
Greg Ogun <gregogun97@gmail.com>
2021-12-15 18:53:56 +0000

    chore: setup packages correctly

---
+++ packages/layout/README.md
@@ -0,0 +1,11 @@
+# `layout`
+
+> TODO: description
+
+## Usage
+
+```
+const layout = require('layout');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/layout/src/box.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Box = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default Box;
