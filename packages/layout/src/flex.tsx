89f095297689a29ee448002196182a29d67bd320
Greg Ogun <gregogun97@gmail.com>
2021-12-23 08:13:54 +0000

    feat(layout): add flex component

---
+++ packages/layout/src/flex.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { styled, ComponentProps, VariantProps } from '@zephyr-ui/theme';
+
+export type FlexProps = ComponentProps<typeof Flex>;
+export type FlexVariants = VariantProps<typeof Flex>;
+/**
+ * Flex is a layout component with a default property of ```display: flex```.
+ * It renders a HTML div element by default.
+ */
+export const Flex = styled('div', {
+  display: 'flex',
+
+  variants: {
+    direction: {
+      row: {
+        flexDirection: 'row',
+      },
+      rowReverse: {
+        flexDirection: 'row-reverse',
+      },
+      column: {
+        flexDirection: 'column',
+      },
+      columnReverse: {
+        flexDirection: 'column-reverse',
+      },
+    },
+
+    space: {
+      even: {
+        justifyContent: 'space-evenly',
+      },
+      between: {
+        justifyContent: 'space-between',
+      },
+    },
+
+    aligned: {
+      true: {
+        alignItems: 'center',
+      },
+    },
+  },
+});
