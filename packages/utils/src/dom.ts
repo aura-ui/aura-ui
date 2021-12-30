7032ac29d395bb135f65de98524615dc1cc20627
Greg Ogun <gregogun97@gmail.com>
2021-12-30 08:37:35 +0000

    chore: function to correctly add aria attributes

--- packages/utils/src/dom.ts
+++ packages/utils/src/dom.ts
@@ -2,3 +2,5 @@ import { Booleanish } from '.';
 
 export const dataAttr = (condition: boolean | undefined) =>
   (condition ? '' : undefined) as Booleanish;
+
+export const ariaAttr = (condition: boolean | undefined) => (condition ? true : undefined);
