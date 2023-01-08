70e87d449a9da8329d37033c62f9facbd7f75e33
Greg Ogun <gregogun97@gmail.com>
2023-01-08 12:14:24 +0000

    feat: bump versions

--- packages/theme/stitches.config.ts
+++ packages/theme/stitches.config.ts
@@ -190,9 +190,9 @@ export const { styled, css, config, theme, createTheme, reset, getCssText, globa
         ...whiteA,
       },
       fonts: {
-        body: '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+        body: 'ui-sans-serif,system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
         heading:
-          '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+          'ui-sans-serif,system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
         mono: 'SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
       },
       fontSizes: {
