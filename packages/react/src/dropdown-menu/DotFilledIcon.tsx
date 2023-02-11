88e66ba794c3a4dd897e9bf41a04f9c9d702e96a
Greg Ogun <65421744+gregogun@users.noreply.github.com>
2023-02-11 01:07:31 +0000

    feat: dropdown enhancements (#68)
    * feat: [WIP] dropdown updates
    
    * fix: radix icons not installed
    
    * feat: update variants

---
+++ packages/react/src/dropdown-menu/DotFilledIcon.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+interface IconProps {
+  width?: number | string;
+  height?: number | string;
+}
+
+export const DotFilledIcon = ({ width = 16, height = 16 }: IconProps) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.875 7.5C9.875 8.81168 8.81168 9.875 7.5 9.875C6.18832 9.875 5.125 8.81168 5.125 7.5C5.125 6.18832 6.18832 5.125 7.5 5.125C8.81168 5.125 9.875 6.18832 9.875 7.5Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+};
