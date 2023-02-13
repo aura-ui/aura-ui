be2a8f95cf22720d8b21aa64c415c2dda585b828
Greg Ogun <65421744+gregogun@users.noreply.github.com>
2023-02-13 18:46:00 +0000

    feat: add radio group ✨ (#74)

---
+++ packages/react/src/radio-group/RadioGroup.stories.tsx
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import { Flex } from '../layout';
+import { Typography } from '../typography';
+import { Radio, RadioGroup } from './RadioGroup';
+
+export default {
+  title: 'Components/Form/RadioGroup',
+  component: RadioGroup,
+};
+
+export const Default = () => (
+  <RadioGroup gap="2">
+    <Radio value="default">Default</Radio>
+  </RadioGroup>
+);
+
+export const Sizes = () => (
+  <RadioGroup gap="2">
+    <Radio size="1" value="default">
+      Size 1
+    </Radio>
+    <Radio size="2" value="default2">
+      Size 2
+    </Radio>
+    <Radio size="3" value="default3">
+      Size 3
+    </Radio>
+  </RadioGroup>
+);
+
+export const Variants = () => (
+  <RadioGroup gap="2">
+    <Radio variant="outline" value="default">
+      Outline
+    </Radio>
+    <Radio variant="solid" value="default2">
+      Solid
+    </Radio>
+  </RadioGroup>
+);
+
+export const IndicatorSizes = () => (
+  <RadioGroup gap="2">
+    <Radio indicatorSize="1" value="default">
+      Indicator Size 1
+    </Radio>
+    <Radio indicatorSize="2" value="default2">
+      Indicator Size 2
+    </Radio>
+    <Radio indicatorSize="3" value="default3">
+      Indicator Size 3
+    </Radio>
+  </RadioGroup>
+);
+
+export const ColorScheme = () => (
+  <RadioGroup gap="2">
+    <Radio colorScheme="tomato" value="tomato">
+      Tomato
+    </Radio>
+    <Radio colorScheme="red" value="red">
+      Red
+    </Radio>
+    <Radio colorScheme="crimson" value="crimson">
+      Crimson
+    </Radio>
+    <Radio colorScheme="pink" value="pink">
+      Pink
+    </Radio>
+    <Radio colorScheme="plum" value="plum">
+      Plum
+    </Radio>
+    <Radio colorScheme="purple" value="purple">
+      Purple
+    </Radio>
+    <Radio colorScheme="violet" value="violet">
+      Violet
+    </Radio>
+    <Radio colorScheme="indigo" value="indigo">
+      Indigo
+    </Radio>
+    <Radio colorScheme="blue" value="blue">
+      Blue
+    </Radio>
+    <Radio colorScheme="sky" value="sky">
+      Sky
+    </Radio>
+    <Radio colorScheme="cyan" value="cyan">
+      Cyan
+    </Radio>
+    <Radio colorScheme="teal" value="teal">
+      Teal
+    </Radio>
+    <Radio colorScheme="mint" value="mint">
+      Mint
+    </Radio>
+    <Radio colorScheme="green" value="green">
+      Green
+    </Radio>
+    <Radio colorScheme="grass" value="grass">
+      Grass
+    </Radio>
+    <Radio colorScheme="lime" value="lime">
+      Lime
+    </Radio>
+    <Radio colorScheme="yellow" value="yellow">
+      Yellow
+    </Radio>
+    <Radio colorScheme="amber" value="amber">
+      Amber
+    </Radio>
+    <Radio colorScheme="orange" value="orange">
+      Orange
+    </Radio>
+  </RadioGroup>
+);
+
+export const RadioCard = () => (
+  <RadioGroup gap="2">
+    <Flex
+      css={{
+        p: '$3',
+        br: '$2',
+        boxShadow: '0 0 0 1px $colors$slate7',
+        maxW: '$30',
+        '&:focus-within': { boxShadow: '0 0 0 1px $colors$blue9' },
+      }}
+    >
+      <Radio indicatorSize="1" value="default">
+        {''}
+      </Radio>
+      <Typography>Option 1</Typography>
+    </Flex>
+    <Flex
+      css={{
+        p: '$3',
+        br: '$2',
+        boxShadow: '0 0 0 1px $colors$slate7',
+        maxW: '$30',
+        '&:focus-within': { boxShadow: '0 0 0 1px $colors$blue9' },
+      }}
+    >
+      <Radio indicatorSize="2" value="default2">
+        {''}
+      </Radio>
+      <Typography>Option 2</Typography>
+    </Flex>
+    <Flex
+      css={{
+        p: '$3',
+        br: '$2',
+        boxShadow: '0 0 0 1px $colors$slate7',
+        maxW: '$30',
+        '&:focus-within': { boxShadow: '0 0 0 1px $colors$blue9' },
+      }}
+    >
+      <Radio indicatorSize="3" value="default3">
+        {''}
+      </Radio>
+      <Typography>Option 3</Typography>
+    </Flex>
+  </RadioGroup>
+);
